fc687652109eb6ffed5a31ac4dc1a1e5b5c63779
ObalSoft <70098100+ObalSoft@users.noreply.github.com>
2020-08-23 17:03:21 +0300

    Update NewClass.java

--- src/obal/NewClass.java
+++ src/obal/NewClass.java
@@ -12,6 +12,6 @@ package obal;
 public class NewClass {
     
     public static void main(String[] args) {
-        System.out.println("new class");
+        System.out.println("new class /n heyyyyyy");
     }
 }
